3af06bc40f8737a5318398eb3f8435f40bf15088
dshah003 <dshah003@terpmail.umd.edu>
2019-03-03 23:49:44 -0500

    Implemented FFT based image evaluation metric.

--- src/tag_detector_node2.cpp
+++ src/tag_detector_node2.cpp
@@ -9,8 +9,12 @@
 #include <ros/ros.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
-#include <image_transport/image_transport.h>
 #include <vector>
+#include <iostream>
+#include <image_transport/image_transport.h>
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
 
 using namespace std;
 using namespace cv;
@@ -18,16 +22,28 @@ using namespace cv;
 void detectRect(Mat& image);
 static double angle(Point pt1, Point pt2, Point pt0);
 static void drawSquares(Mat& image, const vector<vector<Point> >& squares);
+double evaluateImg(Mat image);
+double calcMean(Mat& img);
+
+int qualityThreshold = 2500;
+
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
 	int64 t0, t1;
+    double Sharpness;
 	vector<double> timeRecord;
   	double sec;
 	try {
 		Mat rec_img = cv_bridge::toCvShare(msg, "bgr8")->image;
     	// imshow("Orignal Image", rec_img);
-    	// waitKey(5);
+    	// waitKey(500);
     	t0 = getTickCount();
+        Sharpness = evaluateImg(rec_img); //Evaluate Image Quality.
+        if (Sharpness > qualityThreshold) {
+            ROS_INFO("Quality Measure = %lf | Image is Excessively BLURRED", Sharpness);
+        } else {
+            ROS_INFO("Quality Measure = %lf | Image is Sharp enough!", Sharpness);
+        }
     	detectRect(rec_img);
     	t1 = getTickCount();
     	imshow("Tag Detected", rec_img);
@@ -51,7 +67,7 @@ int main(int argc, char** argv) {
     image_transport::ImageTransport it(nh);
     image_transport::Subscriber sub = it.subscribe("/image_raw", 1, imageCallback);
     ros::spin();
-    destroyAllWindows();
+    // destroyAllWindows();
 }
 
 void detectRect(Mat& ColorImage)
@@ -110,5 +126,40 @@ static void drawSquares( Mat& image, const vector<vector<Point> >& squares )
         int n = (int)squares[i].size();
         polylines(image, &p, &n, 1, true, Scalar(0,255,0), 3, LINE_AA);
     }
-    // imshow("Squares", image);
 }
+
+double evaluateImg(Mat img){
+    cvtColor(img, img, cv::COLOR_RGB2GRAY);
+    int M = getOptimalDFTSize( img.rows ); //Get Row and column sizes
+    int N = getOptimalDFTSize( img.cols );
+    Mat padded; 
+    copyMakeBorder(img, padded, 0, M - img.rows, 0, N - img.cols, BORDER_CONSTANT, Scalar::all(0)); //Padd the image
+
+    Mat planes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
+    Mat complexImg;
+    merge(planes, 2, complexImg);
+
+    dft(complexImg, complexImg);
+
+    split(complexImg, planes);
+    magnitude(planes[0], planes[1], planes[0]);
+    Mat mag = planes[0];
+    double FFT_mean = calcMean(mag); //If the image is blurry, it has high amount of Low frequency 
+                                   //components. Hence the mean is high. This is used as a measure of Sharpness.
+    return FFT_mean; 
+}
+
+double calcMean(Mat& img){
+    int v_bins = 50;
+    int histSize[] = { v_bins };
+    Mat hist;
+    float v_ranges[] = { 0, 255 };
+    const float* ranges[] = { v_ranges };
+    calcHist(&img, 1, 0, cv::Mat(), hist, 1, histSize, ranges, true, false); //histogram calculation
+    Scalar mean, stddev;
+    meanStdDev(hist, mean, stddev);
+    double hist_mean = mean[0];
+    return hist_mean;
+    // std::cout << "Mean: " << mean[0] << "   StdDev: " << stddev[0] << std::endl;
+}
+
